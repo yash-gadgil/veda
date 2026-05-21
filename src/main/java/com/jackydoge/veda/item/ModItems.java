6977cec4391e5001625ef2a121b66688a52e5867
Yash Gadgil <yashgadgil08@gmail.com>
2026-05-21 17:35:59 +0530

    Gada Balancing

--- src/main/java/com/jackydoge/veda/item/ModItems.java
+++ src/main/java/com/jackydoge/veda/item/ModItems.java
@@ -14,7 +14,16 @@ import java.util.function.Function;
 
 public class ModItems {
 
-    public static final Item AMRUT = registerItem("amrut", Item::new, new Item.Properties().component(DataComponents.CONSUMABLE, ModConsumables.AMRUT));
+    public static final Item AMRUT = registerItem("amrut", AmrutItem::new, new Item.Properties()
+            .component(DataComponents.CONSUMABLE, ModConsumables.AMRUT)
+            .rarity(Rarity.EPIC)
+            .stacksTo(16));
+
+    public static final Item COPPER_GADA = registerItem("copper_gada", Item::new, new Item.Properties().sword(ToolMaterial.COPPER,12.0F, -3.5F).durability(400));
+    public static final Item IRON_GADA = registerItem("iron_gada", Item::new, new Item.Properties().sword(ToolMaterial.IRON,13.0F, -3.5F).durability(750));
+    public static final Item GOLDEN_GADA = registerItem("golden_gada", Item::new, new Item.Properties().sword(ToolMaterial.GOLD,10.0F, -3.5F).durability(120));
+    public static final Item DIAMOND_GADA = registerItem("diamond_gada", Item::new, new Item.Properties().sword(ToolMaterial.DIAMOND,15.0F, -3.5F).durability(3650));
+    public static final Item NETHERITE_GADA = registerItem("netherite_gada", Item::new, new Item.Properties().sword(ToolMaterial.NETHERITE,18.0F, -3.5F).durability(5150).fireResistant());
 
     public static final Item COPPER_GADA   = registerItem("copper_gada",   Item::new, new Item.Properties().sword(ToolMaterial.COPPER,    8.0F, -3.4F).durability(1000));
     public static final Item IRON_GADA     = registerItem("iron_gada",     Item::new, new Item.Properties().sword(ToolMaterial.IRON,      8.0F, -3.4F).durability(1500));
